dd9a5652db3e91ff3ad2e52d8a696c522a8f6460
Scraxtastic <nick.u19990@gmail.com>
2021-11-04 01:37:47 +0100

    Added The StartGame Function

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ namespace ReleaseUpdater
     {
         static void Main(string[] args)
         {
+            string jogiGamePath = @"./Jogigame/";
+            string jogiGameFileName = "JogisWayToStreamingGod.exe";
             Console.WriteLine("Starting Updater...");
             ProjectUpdater projectUpdater = new ProjectUpdater("Scraxtastic", "JogisWayToStreamingGod");
-            string updateStatus = projectUpdater.DownloadAndReplace(@"./Jogigame", @"./Jogigame_old",@"./JogiGameUpdate.zip", @"./JogiGame_update", @"./JogiLastDownloadedVersion.txt");
+            string updateStatus = projectUpdater.DownloadAndReplace(jogiGamePath, @"./Jogigame_old",@"./JogiGameUpdate.zip", @"./JogiGame_update", @"./JogiLastDownloadedVersion.txt");
             Console.WriteLine(updateStatus);
             Console.WriteLine("Finished Updater.");
+            Console.WriteLine("Starting Jogigame...");
+            System.Diagnostics.Process.Start(jogiGamePath + jogiGameFileName);
         }
 
     }
